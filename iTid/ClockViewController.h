abf6254ed1e05e3a4cabebe622e2d30d328aac40
Johan <johan.lvr@pluggis.nu>
2012-02-29 17:19:39 +0100

    clock

---
+++ iTid/ClockViewController.h
@@ -0,0 +1,20 @@
+//
+//  ClockViewController.h
+//  clock
+//
+//  Created by Enriquez Guillermo on 7/2/11.
+//  Copyright 2011 Nacho4d. All rights reserved.
+//  See the file License.txt for copying permission.
+//
+
+#import <UIKit/UIKit.h>
+#import "ClockView.h"
+
+@interface ClockViewController : UIViewController 
+{
+}
+
+@property (nonatomic, retain) ClockView *clockView1;
+@property (nonatomic, retain) ClockView *clockView2;
+
+@end
---
+++ iTid/ClockViewController.m
@@ -0,0 +1,88 @@
+//
+//  ClockViewController.m
+//  clock
+//
+//  Created by Enriquez Guillermo on 7/2/11.
+//  Copyright 2011 Nacho4d. All rights reserved.
+//  See the file License.txt for copying permission.
+//
+
+#import "ClockViewController.h"
+
+
+@implementation ClockViewController
+
+@synthesize  clockView1;
+@synthesize  clockView2;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+
+	[clockView1 release];
+    [clockView2 release];
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	
+	//Original clock images was taken from:
+
+	//ClockView with default style
+	clockView1 = [[ClockView alloc] initWithFrame:CGRectMake(50, 250, 200, 200)];
+	clockView1.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+	[self.view addSubview:clockView1];
+    
+    clockView2 = [[ClockView alloc] initWithFrame:CGRectMake(50, 50, 200, 200)];
+	clockView2.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+	[self.view addSubview:clockView2];
+
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+	//start the clock at current time
+
+	[clockView1 start];
+    clockView2.theDate = [NSDate date];
+    [clockView2 updateClock:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+	//stop the clock
+
+	//[clockView2 stop];
+    [clockView1 stop];
+}
+
+- (void)viewDidUnload
+{
+	[super viewDidUnload];
+
+	self.clockView1 = nil;
+    self.clockView2 = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
